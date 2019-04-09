e718ec1dda88fe472e68ec6c9fe7e1e761a569bc
markgmz92 <markgmz92@gmail.com>
2019-04-10 07:05:14 +0800

    Deleted old files

--- question4/calendar/src/utils/randomise.js
+++
@@ -1,9 +0,0 @@
-const randomise = (min, max) => {
-    const random = []
-    for (let i = 0; i < 7; i++) {
-        random.push(Math.ceil(Math.random() * (max - min) + min))
-    }
-    return random
-}
-
-export default randomise
