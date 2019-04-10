cc3c7101d5b59b136de98151d12dd0bd894af8ad
markgmz92 <markgmz92@gmail.com>
2019-04-10 08:05:29 +0800

    Addded formsy

---
+++ question3/form/src/form.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Formsy from 'formsy';
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ...props.user,
+            validationErrors: []
+        }
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleInputChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        const errors = []
+
+        // Validate First Name, Last Name and Email
+        if (!this.state.firstName) errors.push({ key: 0, message: 'First Name Required' })
+        if (!this.state.lastName) errors.push({ key: 1, message: 'Last Name Required' })
+        if (!this.state.email) errors.push({ key: 2, message: 'Email Required' })
+
+        if (!errors.length) {
+            const data = {
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+                email: this.state.email,
+                birthDate: this.state.birthBate,
+                jobTitle: this.state.jobTitle,
+                experienceYears: this.state.experienceYears,
+            }
+
+            // Send PUT Request
+            fetch('https://api.dummyendpoint/me/profile', {
+                    method: 'PUT'
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(data),
+                }).then(res => res.json())
+                .then(response => console.log('Sucess'))
+                .then(response => console.error('Error'))
+        }
+        this.setState({ validationErrors: errors })
+    }
+
+    render() {
+        let status;
+        if (this.state.validationErrors.length) {
+            status = (<div className="alert alert-danger">
+        {this.state.validationErrors.map(item => <div key={item.key}>{item.message}</div>)}
+        </div>)
+        }
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+            {status}
+            <div className="form-group">
+              <label>First Name</label>
+              <input className="form-control" name="firstName" onChange={this.handleInputChange} value={this.state.firstName} />
+            </div>
+            <div className="form-group">
+              <label>Last Name</label>
+              <input className="form-control" name="lastName" onChange={this.handleInputChange} value={this.state.lastName} />
+            </div>
+            <div className="form-group">
+              <label>Email</label>
+              <input className="form-control" name="email" onChange={this.handleInputChange} value={this.state.email} />
+            </div>
+            <div className="form-group">
+              <label>Birth Date</label>
+              <input className="form-control" type="datetime-local" name="birthDate" onChange={this.handleInputChange} value={this.state.birthDate} />
+            </div>
+            <div className="form-group">
+              <label>Preferred Job Title</label>
+              <input className="form-control" name="jobTitle" onChange={this.handleInputChange} value={this.state.jobTitle} />
+            </div>
+            <div className="form-group">
+              <label>Number of Years Experience</label>
+              <input className="form-control" type="number" name="experienceYears" onChange={this.handleInputChange} value={this.state.experienceYears} />
+            </div>
+            <div className="form-group">
+              <button>Submit</button>
+            </div>
+          </form>
+        )
+    }
+}
+
+export form
+default
--- question3/form/src/index.js
+++ question3/form/src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import form from './form'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            user: {
+                firstName: '',
+                lastName: '',
+                email: '',
+                birthDate: '',
+                jobTitle: '',
+                experienceYears: 0
+            }
+        }
+    }
+    render() {
+        return (
+            <div className="container">
+            <div className="row">
+              <div className="col-12 col-md-8 offset-md-2 jumbotron">
+                <h1>User Form</h1>
+                <Form user={this.state.user} />
+              </div>
+            </div>
+          </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
